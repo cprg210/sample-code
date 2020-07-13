0fe076c2c5c72c99f805297a3ce7d9bef614cd01
Tony Grimes <acidtone@tonygrimes.com>
2020-07-13 07:34:52 -0600

    added animals demo apps

---
+++ backend/2-animals-pug/app.js
@@ -0,0 +1,33 @@
+// import modules
+const path = require('path');
+const express = require('express');
+const animals = require('./animals');
+
+// create express app
+const app = express();
+app.set('view engine', 'pug');
+
+// automatically check if requested file is found in /public
+// if yes, return that file as a response to the browser
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(request, response){
+  response.render('index.pug',{});
+})
+
+// app.get('/animals', function(request, response){
+//   response.json(animals);
+// })
+
+// if no, send a 404 error as a response to the browser
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send('404: File Not Found');
+});
+
+// start up server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
