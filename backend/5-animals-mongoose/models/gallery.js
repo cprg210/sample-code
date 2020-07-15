df02cae124cc62377134f42134bc2c5f661e19b8
Tony Grimes <acidtone@tonygrimes.com>
2020-07-15 07:41:47 -0600

    added Day 15 final code

---
+++ backend/5-animals-mongoose/models/gallery.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Step 1: Define our Schema
+// See: https://mongoosejs.com/docs/guide.html
+/*
+"Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection."
+*/
+const gallerySchema = new mongoose.Schema(
+  {
+    id:           Number,
+    title:        String,
+    source:       String,
+    fileName:     String,
+    attribution:  {
+      source: String,
+      credit: String,
+      url:  String
+    }
+  }
+);
+
+// Compile and export our model using the above Schema.
+// See: https://mongoosejs.com/docs/models.html 
+
+module.exports = mongoose.model('Image', gallerySchema);
+// Important: The first argument of mongoose.model() is the singular name of the collection your model is for. 
+// ** Mongoose automatically looks for the plural, lowercased version of your model name. **"
+// In our example, we name our model 'Definition' and mongoose will automatically look for the collection 'definitions' 
